806c8295488ccd679ebb444565ab2b342cba7226
keigo <89068705+gongitsune@users.noreply.github.com>
2024-07-05 10:25:09 +0900

    開発用コンテナを作成

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,10 +1,16 @@
+using TodoService = GrpcService.Services.TodoService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<TodoService>();
+
+if (app.Environment.IsDevelopment()) app.MapGrpcReflectionService();
 
 app.Run();
---
+++ GrpcService/Services/TodoService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using Proto.Todo.V1;
+
+namespace GrpcService.Services;
+
+public class TodoService : Proto.Todo.V1.TodoService.TodoServiceBase
+{
+    public override Task<TodoServiceCreateResponse> Create(TodoServiceCreateRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new TodoServiceCreateResponse
+        {
+            Title = "Hello World!",
+            Description = "This is a new todo item.",
+            Done = true
+        });
+    }
+}
