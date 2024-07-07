c58793263db3f1f56dfc5a7a7df242cce64468ce
keigo <89068705+gongitsune@users.noreply.github.com>
2024-07-08 06:57:27 +0900

    認証処理を実装

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,14 +1,47 @@
+using System.Security.Claims;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using TodoService = GrpcService.Services.TodoService;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var firebaseOpt = new AppOptions
+{
+    Credential = GoogleCredential.GetApplicationDefault(),
+    ProjectId = "u22-2024"
+};
+FirebaseApp.Create(firebaseOpt);
+
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    {
+        policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireClaim(ClaimTypes.Name);
+    });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.Authority = $"https://securetoken.google.com/{firebaseOpt.ProjectId}";
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = $"https://securetoken.google.com/{firebaseOpt.ProjectId}",
+            ValidateAudience = true,
+            ValidAudience = firebaseOpt.ProjectId,
+            ValidateLifetime = true
+        };
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGrpcService<TodoService>();
 
 if (app.Environment.IsDevelopment()) app.MapGrpcReflectionService();
--- GrpcService/Services/TodoService.cs
+++ GrpcService/Services/TodoService.cs
@@ -1,17 +1,21 @@
 ﻿using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
 using Proto.Todo.V1;
 
 namespace GrpcService.Services;
 
 public class TodoService : Proto.Todo.V1.TodoService.TodoServiceBase
 {
+    [Authorize]
     public override Task<TodoServiceCreateResponse> Create(TodoServiceCreateRequest request, ServerCallContext context)
     {
+        var user = context.GetHttpContext().User;
+        var uid = user.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value;
         return Task.FromResult(new TodoServiceCreateResponse
         {
-            Title = "Hello World!",
-            Description = "This is a new todo item.",
-            Done = true
+            Title = $"Todo: {request.Title}, User: {uid ?? "Unknown"}",
+            Description = string.IsNullOrEmpty(request.Description) ? "No description" : request.Description,
+            Done = false
         });
     }
 }
