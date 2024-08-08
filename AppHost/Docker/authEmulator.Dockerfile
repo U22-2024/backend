949341352e70e790ad38eecfe4a4e1881920fb45
kara <g22307@kagawa.kosen-ac.jp>
2024-08-08 20:04:20 +0900

    FirebaseEmulatorを開発時のオーケストレーションに追加

---
+++ AppHost/Docker/authEmulator.Dockerfile
@@ -0,0 +1,10 @@
+﻿FROM node:alpine
+
+# 必要なパッケージをインストール
+RUN apk update \
+ && apk --no-cache add openjdk17-jre-headless \
+ && rm -rf /var/cache/apk/* \
+ && npm install -g firebase-tools
+
+WORKDIR /opt/firebase
+CMD [ "firebase", "emulators:start", "--project", "u22-2024" ]
--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,6 +2,16 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Firebase Auth Emulatorを開発時のみ追加
+#if DEBUG
+builder.AddDockerfile("firebase", "Docker", "authEmulator.Dockerfile")
+    .WithBindMount("./Docker/firebase/.firebaserc", "/opt/firebase/.firebaserc")
+    .WithBindMount("./Docker/firebase/firebase.json", "/opt/firebase/firebase.json")
+    .WithExternalHttpEndpoints()
+    .WithHttpEndpoint(port: 9099, targetPort: 9099, name: "auth-emulator")
+    .WithHttpEndpoint(port: 4000, targetPort: 4000, name: "auth-ui");
+#endif
+
 // Postgresql を追加
 var postgres = builder.AddPostgres("postgres");
 
