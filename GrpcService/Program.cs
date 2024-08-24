a84544076abeac2ba34811b24acbc80a104494bf
Boson <boson_typing@outlook.jp>
2024-08-25 05:39:56 +0900

    一言メッセージを返すGreetServiceの実装

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,7 +1,10 @@
+using Spire.Xls;
+using System.Data;
 using GrpcService;
 using GrpcService.Extensions;
 using GrpcService.Services;
 using Microsoft.EntityFrameworkCore;
+using GrpcService.Models.Greet;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +29,8 @@ app.MapGrpcService<RemindService>();
 app.MapGrpcService<RemindGroupService>();
 app.MapGrpcService<HealthCheckService>();
 app.MapGrpcService<RemindTemplateService>();
+app.MapGrpcService<GreetService>();
+
 
 if (app.Environment.IsDevelopment()) app.MapGrpcReflectionService();
 using (var scope = app.Services.CreateScope())
@@ -33,6 +38,29 @@ await using (var dbCtx = scope.ServiceProvider.GetRequiredService<AppDbContext>(
 {
     var strategy = dbCtx.Database.CreateExecutionStrategy();
     await strategy.ExecuteAsync(() => dbCtx.Database.EnsureCreatedAsync());
+
+    // 一言メッセージを読み込んでデータベースに保存する
+    Workbook wb = new Workbook();
+    wb.LoadFromFile("../mother_hitokoto.xlsx");
+
+    Worksheet worksheet = wb.Worksheets[0];
+
+    for (int row = 1; row <= worksheet.LastRow; row++)
+    {
+        CellRange range = worksheet.Range[row, 1];
+        string cellValue = range.Text == null ? string.Empty : range.Text.ToString();
+        GreetModel greet = new GreetModel { Id = row, Message = cellValue };
+
+        dbCtx.Greets.Add(greet);
+    }
+
+    // リソースを解放する
+    wb.Dispose();
+
+    // DBへ書き込み
+    await dbCtx.SaveChangesAsync();
 }
 
+
 app.Run();
+
