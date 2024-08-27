6fa860064e2af0c87c4c1a04ed709422fc77d4c0
jet1202 <jet86488@gmail.com>
2024-08-27 19:06:31 +0900

    テスト用プログラムを削除

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,4 +1,3 @@
-using Event.V1;
 using GrpcService;
 using GrpcService.API;
 using GrpcService.Extensions;
@@ -6,7 +5,6 @@ using GrpcService.Models.Greet;
 using GrpcService.Services;
 using Microsoft.EntityFrameworkCore;
 using Spire.Xls;
-using DateTime = Event.V1.DateTime;
 using EventMaterialService = GrpcService.Services.EventMaterialService;
 using EventService = GrpcService.Services.EventService;
 
@@ -66,29 +64,6 @@ await using (var dbCtx = scope.ServiceProvider.GetRequiredService<AppDbContext>(
 
     // DBへ書き込み
     await dbCtx.SaveChangesAsync();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    GetTimeTable timeTable = scope.ServiceProvider.GetRequiredService<GetTimeTable>();
-    EventMaterial eventMaterial = new EventMaterial
-    {
-        IsOut = true,
-        FromPos = new Pos
-        {
-            Lat = 34.233670595665366,
-            Lon = 133.6356901033301
-        },
-        DestinationPos = new Pos
-        {
-            Lat = 34.35271166267611,
-            Lon = 134.0470596463827
-        },
-        MoveType = MoveType.Car,
-        StartTime = new DateTime{Year = 2022, Month = 1, Day = 1, Hour = 0, Minute = 0},
-        EndTime = new DateTime{Year = 2022, Month = 1, Day = 1, Hour = 12, Minute = 0}
-    };
-    var response = await timeTable.GetTimeTableList(eventMaterial, true);
 }
 
 app.Run();
