4f97f4d428971cc31831d977f809edd76be79e35
Boson <boson_typing@outlook.jp>
2024-08-27 18:04:41 +0900

    テスト用のやつをコメントアウト

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -44,22 +44,22 @@ await using (var dbCtx = scope.ServiceProvider.GetRequiredService<AppDbContext>(
 using (var scope = app.Services.CreateScope())
 {
 
-    var ai = scope.ServiceProvider.GetRequiredService<GetPlace>();
-    var pos = new Pos();
-    pos.Lat = 34.23404579573394;
-    pos.Lon = 133.6358061308664;
-    ai.GetTextPos("イオン", pos);
+    // var ai = scope.ServiceProvider.GetRequiredService<GetPlace>();
+    // var pos = new Pos();
+    // pos.Lat = 34.23404579573394;
+    // pos.Lon = 133.6358061308664;
+    // ai.GetTextPos("イオン", pos);
 
     // var ai = scope.ServiceProvider.GetRequiredService<PredictEvent>();
     // var eventMaterial = new EventMaterial();
     // EventMaterial after = await ai.PredictEventMaterial("明日の9時に友達とバイクでイオンに行く。今日は2024/08/24", eventMaterial);
 
-    var api = scope.ServiceProvider.GetRequiredService<GetRainfall>();
-    var location = new Location();
-    location.latitude = 34.23404579573394;
-    location.longitude = 133.6358061308664;
+//     var api = scope.ServiceProvider.GetRequiredService<GetRainfall>();
+//     var location = new Location();
+//     location.latitude = 34.23404579573394;
+//     location.longitude = 133.6358061308664;
 
-   Console.WriteLine(await api.GetListRainfall(location));
+//    Console.WriteLine(await api.GetListRainfall(location));
 }
 
 app.Run();
